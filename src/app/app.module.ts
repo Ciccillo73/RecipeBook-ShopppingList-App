9d8c19865279ae19f182369a46b7e2eb95599af1
Francesco <med.dama@gmail.com>
2020-08-01 17:51:40 +0100

    Add rendering component recipe list and recipe detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeComponent } from './recipes/recipe/recipe.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
@@ -16,7 +15,6 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeComponent,
     RecipeDetailComponent,
     RecipeListComponent,
     RecipeItemComponent,
